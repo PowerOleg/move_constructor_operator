c0d2510d75a49c00e740ba366acd71202ac935b0
PowerOleg <geraskin.oleg@mail.ru>
2024-07-07 14:19:21 +0300

    task1 is done

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -4,31 +4,25 @@
 
 
 template<class T>
-void move_vectors(T dest, T&& src)
+void move_vectors(T&& dest, T&& src)
 {
-	std::string tmp(std::move(src));
-	//T temp = src;
-	//&&src = nullptr;
-	//dest = temp;
+	dest = std::move(src);
 }
 
-/*template<> void move_vectors(std::vector<std::string> dest, std::vector<std::string>&& src)
+
+/*template<> void move_vectors(std::vector<std::string> *&dest, std::vector<std::string> *&src)
 {
-	std::vector <std::string> temp = src;
-	//src.erase(src.begin(), src.end());
-	delete[] src.data();
-	dest = temp;
+	*dest = std::move(src);
 }*/
 
 int main(int argc, char** argv)
 {
 	std::string first = {"first"};
-	std::string second = { "second" };
-	//std::string** second_link_link = &&second;
-	move_vectors(first, "second");
+	std::string second = {"second"};
+	move_vectors(std::move(first), std::move(second));
 
-	//std::vector<std::string> one = { "test_string1", "test_string2" };
-	//std::vector<std::string> two;
-	//move_vectors(two, one);
+	std::vector<std::string> one = { "test_string1", "test_string2" };
+	std::vector<std::string> two;
+	move_vectors(std::move(two), std::move(one));
 	return 0;
 }
