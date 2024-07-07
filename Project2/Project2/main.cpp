0e63295cba3fb21c6d7f618482c535a519511f29
PowerOleg <geraskin.oleg@mail.ru>
2024-07-07 19:13:47 +0300

    task2 is done with error

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 class Big_integer 
 {
@@ -22,11 +23,54 @@ public:
 		delete[] result_char;
 	}
 
+	Big_integer(const Big_integer& other) : result_char{other.result_char}//не понимаю почему есть доступ до other.result_char ведь private?
+	{}
+	Big_integer& operator=(const Big_integer& other)
+	{
+		auto new_big_int = new Big_integer(other);
+		return /**this=new_big*/*new_big_int;
+	}
 
+	Big_integer(Big_integer&& other) noexcept : result_char{std::exchange(other.result_char, nullptr)}
+	{}
+	Big_integer& operator=(Big_integer&& other) noexcept
+	{
+		std::swap(result_char, other.result_char);
+		return *this;
+	}
+
+	Big_integer& operator+(Big_integer& right)
+	{
+		long left_value = std::stol(this->result_char);
+		//long right_value = std::stol(right.result_char);
+
+		delete[] this->result_char;
+		*this = std::move(right);
+
+
+		long right_value = std::stol(this->result_char);
+
+		long result = left_value + right_value;
+		std::string str = std::to_string(result);
+
+		char* result_char_new = new char[str.length() + 1];
+		for (size_t i = 0; i < str.length() + 1; i++)
+		{
+			result_char_new[i] = str[i];
+		}
+		delete[] this->result_char;
+		this->result_char = std::move(result_char_new);
+		return *this;
+	}
+	friend std::ostream& operator<<(std::ostream& os, Big_integer& big_integer);
 private:
 	//std::vector<char> result;
 	char* result_char;
 };
+std::ostream& operator<<(std::ostream& os, Big_integer& big_integer)
+{
+	return os << big_integer.result_char;
+}
 
 template<class T>
 void move_vectors(T& dest, T& src)
@@ -38,9 +82,7 @@ int main(int argc, char** argv)
 {
 	auto number1 = Big_integer("114575");
 	auto number2 = Big_integer("78524");
-	//auto result = number1 + number2;
-	//std::cout << result; // 193099
-
-
+	auto result = number1 + number2;
+	std::cout << result; // 193099
 	return 0;
 }
